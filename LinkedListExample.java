8bc0dfbc2bcad7c6ee1829c4edf69385a644930c
developersmita767 <156692415+developersmita767@users.noreply.github.com>
2024-07-07 19:19:23 +0530

    Add files via upload

---
+++ LinkedListExample.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class LinkedListExample{
+    public static void main(String[] ar){
+        LinkedList<String> strList=new LinkedList<>();
+        strList.add("IBM");
+        strList.add("TCS");
+        System.out.println(strList);
+    }
+}
